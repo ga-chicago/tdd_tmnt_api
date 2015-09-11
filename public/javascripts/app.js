83b7d57fc09b0f469b86332543fc7852ff3784fe
James Traver <jtraverjr@gmail.com>
2015-09-11 15:41:54 -0500

    final pass friday

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,1 +1,17 @@
 // app.js
+var turtlesApp = angular.module('turtlesApp', []);
+
+turtlesApp.controller('turtlesCtrl', function($scope, $http) {
+
+  //www.someplace.net/api/turtles
+  $scope.url = '/turtles'; // api for our turtles
+
+  $scope.fetch = function() {
+    $http.get($scope.url).success(function(data) {
+      $scope.data = data;
+    });
+  }
+
+  $scope.fetch();
+
+});
